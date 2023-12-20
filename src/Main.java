3d197e3dde29f095a1cf563c5f30e0db32402749
Adilhon06 <adubaichik2006@gmail.com>
2023-12-20 17:53:02 +0600

    changed

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        getBack(34, 20);
-        getBack(23, 10);
-        getBack(15, -20);
-        getBack(16, 16);
-        getBack(48, 36);
+        System.out.println(getBack(24, 47));
+        System.out.println(getBack(41, 34));
+        System.out.println(getBack(35, 13));
+        System.out.println(getBack(76, 24));
+        System.out.println(getBack(45, -15));
     }
-    public static String getBack(int age, int temperature){
 
-        if (age >= 20 && age <=40 && temperature >= -20 && temperature <= 30){
-            System.out.println("Можно идти гулять");
-        } else if (age <= 20 && temperature >= 0 && temperature <= 28){
-            System.out.println("Можно идти гулять");
+    public static String getBack(int age, int temperature) {
+        if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
+            return "Можно идти гулять";
+        } else if (age < 20 && temperature >= 0 && temperature <= 28) {
+            return "Можно идти гулять";
         } else if (age >= 45 && temperature >= -10 && temperature <= 25) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         } else {
-            System.out.println("Оставайся дома");
             return "Оставайся дома";
         }
-        return "Можно идти гулять";
     }
 }
